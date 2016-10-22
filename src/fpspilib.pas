aa2b1a066ee5acc9c31175c68b6211ab15149fcd
Simon Ameis <simon.ameis@web.de>
2016-10-22 18:57:38 +0200

    Added support for ADC MCP3204 and MCP3204 Refactored the coding to a new base class TMCP3X0X for all similar devices.

--- src/fpspilib.pas
+++ src/fpspilib.pas
@@ -37,19 +37,26 @@ uses
   Classes, SysUtils, fpspi, fpadc;
 
 type
-  { TMCP300X }
 
-  TMCP300X = class(TADConverter)
-  private
+  TMCP3X0X = class(TADConverter)
+  protected                     
     fBus: TSPIDevice;
-  protected
-    class function GetMaxValue: Longword; static; override;
-    function InternalGetValue(Single: Boolean; Channel: Byte): Longword;
-    function GetValue(Index: Longword): Longword; override;
-    function GetDifferentialValue(Index: Longword): Longword; override;
-    class function GetSupportsDifferentialValue: Boolean; override;
+    function GetValue(Index: Longword): Longword; override; inline;
+    function GetDifferentialValue(Index: Longword): Longword; override; inline;
+    class function GetSupportsDifferentialValue: Boolean; override; inline;
+    function InternalGetValue(Single: Boolean; Channel: Byte
+      ): Longword; virtual; abstract;
   public
     constructor Create(aBus: TSPIDevice);
+  end;
+
+  { TMCP300X }
+
+  TMCP300X = class(TMCP3X0X)
+  protected                 
+    class function GetMaxValue: Longword;
+    function InternalGetValue(Single: Boolean; Channel: Byte
+      ): Longword; override;
   end;
 
   { TMCP3008 }
@@ -66,7 +73,106 @@ type
     class function GetCount: Longword; static; override;
   end;
 
+  TMCP320X = class(TMCP3X0X)
+  protected
+    class function GetMaxValue: Longword; static; override;
+    function InternalGetValue(Single: Boolean; Channel: Byte): Longword; override;
+  end;
+
+  { TMCP3208 }
+
+  TMCP3208 = class(TMCP320X)
+  protected
+    class function GetCount: Longword; static; override;
+  end;
+
+  { TMCP3004 }
+
+  TMCP3204 = class(TMCP320X)
+  protected
+    class function GetCount: Longword; static; override;
+  end;
+
+
 implementation
+
+{ TMCP3204 }
+
+class function TMCP3204.GetCount: Longword;
+begin
+  Result := 4;
+end;
+
+{ TMCP3208 }
+
+class function TMCP3208.GetCount: Longword;
+begin
+  Result := 8;
+end;
+
+{ TMCP320X }
+
+class function TMCP320X.GetMaxValue: Longword;
+begin
+  // 12 bit
+  Result := $FFF;
+end;
+
+function TMCP320X.InternalGetValue(Single: Boolean; Channel: Byte): Longword;
+const
+  start_bit = $0400;
+  sgl_bit   = $0200;
+var
+  wbuf: array [0..1] of Byte;
+  wword: word absolute wbuf; //< access wbuf as word
+  rbuf: array[0..3] of Byte;
+  rlong: Longword absolute rbuf;
+begin
+  if (Channel > (Count - 1)) then
+    raise EADCError.CreateFmt(sChannelOutOfBounds, [Channel]);
+  // start bit and channel
+  wword := start_bit or (Channel shl 6);
+  // set single bit, if requested; otherwise differential input is used
+  if not single then
+    wword := wword or sgl_bit;
+
+  // initialize read buffer
+  rlong := 0;
+  // pass second byte of longword (= 3 bytes)
+  fBus.ReadAndWrite(wbuf[0], Length(wbuf), rbuf[1], Length(rbuf)-1);
+  // first byte of longword hasn't been used as read buffer and is still 0
+  // second byte of longowrd / first read byte is ignored/garbage
+  // lowest 4 bits of second byte and third byte
+  Result := rlong and $00000FFF;
+end;
+
+{ TMCP3X0X }
+
+function TMCP3X0X.GetValue(Index: Longword): Longword;
+begin
+  Result := InternalGetValue(True, Index);
+end;
+
+function TMCP3X0X.GetDifferentialValue(Index: Longword): Longword;
+begin
+  // each 2 single input channels make one differential channel
+  if (Channel > ((Count div 2) - 1) then
+    raise EADCError.CreateFmt(sChannelOutOfBounds, [Channel]);
+  Result := InternalGetValue(False, Index);
+end;
+
+class function TMCP3X0X.GetSupportsDifferentialValue: Boolean;
+begin
+  Result := True;
+end;
+
+constructor TMCP3X0X.Create(aBus: TSPIDevice);
+begin
+  fBus := aBus;
+  fBus.BitsPerWord := 8;
+  fBus.Mode := SPI_MODE_0;
+  fbus.LSBFirst := False;
+end;
 
 { TMCP3004 }
 
@@ -105,34 +211,11 @@ begin
   else
     wbuf[1] := diff_bit or (Channel shl 4);
   FillChar(rbuf[0], sizeof(rbuf), 0);
-  fBus.ReadAndWrite(wbuf[0], length(wbuf), rbuf[0], 3);
+  fBus.ReadAndWrite(wbuf[0], Length(wbuf), rbuf[0], Length(rbuf));
   // first read byte is ignored/garbage
   // lowest 2 bits of second byte and third byte
   Result := ((rbuf[1] and $03) shl 8) + rbuf[2];
 end;
 
-function TMCP300X.GetValue(Index: Longword): Longword;
-begin
-  Result := InternalGetValue(True, Index);
-end;
-
-function TMCP300X.GetDifferentialValue(Index: Longword): Longword;
-begin
-  Result := InternalGetValue(False, Index);
-end;
-
-class function TMCP300X.GetSupportsDifferentialValue: Boolean;
-begin
-  Result := True;
-end;
-
-constructor TMCP300X.Create(aBus: TSPIDevice);
-begin
-  fBus := aBus;
-  fBus.BitsPerWord := 8;
-  fBus.Mode := SPI_MODE_0;
-  fbus.LSBFirst := False;
-end;
-
 end.
-
+
